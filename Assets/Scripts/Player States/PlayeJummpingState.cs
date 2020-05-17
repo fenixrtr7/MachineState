d8edd75149cb296876578fe1c53dde1a4a9eca89
Arturo E <ing.arturoescamilla@outlook.com>
2020-05-16 23:54:16 -0500

    v1

---
+++ Assets/Scripts/Player States/PlayeJummpingState.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayeJummpingState : PlayerBaseState
+{
+    public override void EnterState(PlayerController_FSM player)
+    {
+        player.SetExpression(player.jumpingSprite);
+    }
+
+    public override void OnCollisionEnter(PlayerController_FSM player)
+    {
+        player.TransitionToState(player.IdleState);
+    }
+
+    public override void Update(PlayerController_FSM player)
+    {
+        if (Input.GetButtonDown("Duck"))
+        {
+            player.TransitionToState(new PlayerSpinnigState());
+        }
+    }
+}
---
+++ Assets/Scripts/Player States/PlayerBaseState.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public abstract class PlayerBaseState
+{
+    public abstract void EnterState(PlayerController_FSM player);
+
+    public abstract void Update(PlayerController_FSM player);
+
+    public abstract void OnCollisionEnter(PlayerController_FSM player);
+}
